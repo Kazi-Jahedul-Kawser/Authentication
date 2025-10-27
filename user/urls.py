a8f482b11daa1cbc833e583a9001c988a3d66701
unknown <jkrafi149@gmail.com>
2025-10-28 02:07:35 +0600

    email verification on both registration and change password done

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
-from user.views import ProfileView, UserCreateView, UserLoginView, VerifyOTPView, home, userlogout, UserUpdateView, ChangePasswordView
+from django.contrib.auth import views as auth_views
+from user.views import ProfileView, UserCreateView, UserLoginView, VerifyOTPView, home, userlogout, UserDataUpdateView
 urlpatterns = [
     path('', home, name="home"),
     path('profile/', ProfileView.as_view(), name="profile"),
-    path('registration/', UserCreateView.as_view(), name='registration'),
-    path('verify_otp/<int:user_id>/',VerifyOTPView.as_view(), name='verify_otp'),
     path('login/', UserLoginView.as_view(), name="login"),
     path('logout/', userlogout, name="logout"),
-    path('update/',UserUpdateView.as_view(), name="update"),
-    path('change_password/',ChangePasswordView.as_view(), name="change_password"),
+    path('update/',UserDataUpdateView.as_view(), name="update"),
+    path('change_password/',auth_views.PasswordChangeView.as_view(template_name = "change_password.html"), name="change_password"),
+    # for registration with email verification
+    path('registration/', UserCreateView.as_view(), name='registration'),
+    path('verify_otp/<int:user_id>/',VerifyOTPView.as_view(), name='verify_otp'),
+    # for Password Reset
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
--- user/views.py
+++ user/views.py
@@ -104,7 +104,7 @@ class UserLoginView(LoginView):
         return context
     
 @method_decorator(login_required,name='dispatch')
-class UserUpdateView(UpdateView):
+class UserDataUpdateView(UpdateView):
     model = User
     form_class = UpdateUserData
     template_name = 'registration.html'
@@ -122,27 +122,27 @@ class UserUpdateView(UpdateView):
         context = super().get_context_data(**kwargs)
         context["type"] = "Update Profile" 
         return context
-@method_decorator(login_required,name='dispatch')    
-class ChangePasswordView(FormView):
-    form_class = PasswordChangeForm
-    template_name = "registration.html"
-    success_url = reverse_lazy('profile')
+# @method_decorator(login_required,name='dispatch')    
+# class ChangePasswordView(FormView):
+#     form_class = PasswordChangeForm
+#     template_name = "registration.html"
+#     success_url = reverse_lazy('profile')
     
-    def get_form_kwargs(self):
-        kargs = super().get_form_kwargs()
-        kargs['user'] = self.request.user
-        return kargs
-    def form_valid(self, form):
-        form.save()
-        messages.success(self.request, "Successfully Updated Password")
-        return super().form_valid(form)
-    def form_invalid(self, form):
-        messages.error(self.request, "Plese enter Valid Information")
-        return super().form_invalid(form)
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["type"] = "Password" 
-        return context
+#     def get_form_kwargs(self):
+#         kargs = super().get_form_kwargs()
+#         kargs['user'] = self.request.user
+#         return kargs
+#     def form_valid(self, form):
+#         form.save()
+#         messages.success(self.request, "Successfully Updated Password")
+#         return super().form_valid(form)
+#     def form_invalid(self, form):
+#         messages.error(self.request, "Plese enter Valid Information")
+#         return super().form_invalid(form)
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["type"] = "Password" 
+#         return context
     
     
 
