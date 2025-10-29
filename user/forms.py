a0ec0afa3ea3826ee55843a50a3025e722426505
unknown <jkrafi149@gmail.com>
2025-10-29 09:43:49 +0600

    make some upgrade

--- user/forms.py
+++ user/forms.py
@@ -3,8 +3,8 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm, Password
 from django.contrib.auth.models import User
 from django import forms
 class Registration(UserCreationForm):
-    first_name = forms.CharField(max_length=200, required=True)
-    email = forms.EmailField(required=False)
+    first_name = forms.CharField(max_length=200, required=True, )
+    email = forms.EmailField(required=True)
     class Meta:
         model = CustomUser
         fields = ['username', 'first_name', 'last_name', 'email']
